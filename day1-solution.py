64f2625cd3ab4cfcae5b67005255ccac0bc18d61
Sam Byers <sambyers@users.noreply.github.com>
2022-12-12 11:36:47 -0500

    finished second part of day1

--- day1-solution.py
+++ day1-solution.py
@@ -41,18 +41,30 @@ def find_largest_sum(lists):
     return max_list, max_sum
 
 
+def sum_top_three(input_list):
+    sum_list = []
+    for i in input_list:
+        sum_list.append(sum(i))
+    sum_list.sort(reverse=True)
+    return sum(sum_list[:3])
+
+
 def main():
+    # -i means use the puzzle input
+    # -e means use example puzzle input
     arg = sys.argv[1]
     if arg == "-e":
-        with open("1-1-example-input") as fh:
+        with open("day1-example-input") as fh:
             lists_str = fh.read()
     elif arg == "-i":
-        with open("1-1-input") as fh:
+        with open("day1-input") as fh:
             lists_str = fh.read()
 
     lists = format_text_to_lists(lists_str)
     sum = find_largest_sum(lists)
-    print(sum)
+    sum_top3 = sum_top_three(lists)
+    print(f"First answer: {sum}")
+    print(f"Second answer: {sum_top3}")
 
 
 if __name__ == "__main__":
